a99871a18ead999ad6b174c62ed21061e4e3f89d
Daniel Bradberry <daniel.bradberry@mwrinfosecurity.com>
2013-01-16 14:18:56 +0000

    cleaning the implementation of InvalidMessageException

--- src/com/mwr/cinnibar/api/InvalidMessageException.java
+++ src/com/mwr/cinnibar/api/InvalidMessageException.java
@@ -1,5 +1,7 @@
 package com.mwr.cinnibar.api;
 
+import java.util.Locale;
+
 import com.mwr.cinnibar.api.Protobuf.Message;
 
 public class InvalidMessageException extends RuntimeException {
@@ -11,14 +13,12 @@ public class InvalidMessageException extends RuntimeException {
 		this.invalid_message = invalid_message;
 	}
 	
-	public InvalidMessageException(String message, Message invalid_message) {
-		super(message);
-		
-		this.invalid_message = invalid_message;
-	}
-	
 	public Message getInvalidMessage() {
 		return this.invalid_message;
+	}
+	
+	public String toString() {
+		return String.format(Locale.ENGLISH, "Invalid message: %s", this.invalid_message.toString());
 	}
 
 }
