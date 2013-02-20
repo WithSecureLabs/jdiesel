b04731dcb778df57ea423dc4583a9eff8e026586
Daniel Bradberry <daniel.bradberry@mwrinfosecurity.com>
2013-02-20 08:59:00 +0000

    tweaking AbstractSession to call the onSessionStarted and onSessionStopped callbacks on an AbstractSessionCollection

--- src/com/mwr/cinnibar/connection/AbstractSession.java
+++ src/com/mwr/cinnibar/connection/AbstractSession.java
@@ -2,6 +2,8 @@ package com.mwr.cinnibar.connection;
 
 import java.math.BigInteger;
 import java.security.SecureRandom;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -9,10 +11,20 @@ import com.mwr.cinnibar.api.InvalidMessageException;
 import com.mwr.cinnibar.api.Protobuf.Message;
 
 public abstract class AbstractSession extends Thread {
+	
+	public interface OnSessionStatusListener {
+		
+		public void onSessionStarted(AbstractSession session);
+		
+		public void onSessionStopped(AbstractSession session);
+		
+	}
 
 	private BlockingQueue<Message> messages = new LinkedBlockingQueue<Message>();
 	private String session_id = null;
 	public volatile boolean running = false;
+	
+	private Set<OnSessionStatusListener> on_session_status_listeners = new HashSet<OnSessionStatusListener>();
 	
 	public AbstractSession() {
 		this.session_id = this.generateSessionId();
@@ -20,6 +32,10 @@ public abstract class AbstractSession extends Thread {
 
 	protected AbstractSession(String session_id) {
 		this.session_id = session_id;
+	}
+	
+	public void addOnSessionStatusListener(OnSessionStatusListener listener) {
+		this.on_session_status_listeners.add(listener);
 	}
 	
 	public void deliverMessage(Message message) {
@@ -35,10 +51,17 @@ public abstract class AbstractSession extends Thread {
 	}
 	
 	protected abstract Message handleMessage(Message message) throws InvalidMessageException;
+	
+	public void removeOnSessionStatusListener(OnSessionStatusListener listener) {
+		this.on_session_status_listeners.remove(listener);
+	}
 		
 	@Override
 	public void run() {
 		this.running = true;
+		
+		for(OnSessionStatusListener l : this.on_session_status_listeners)
+			l.onSessionStarted(this);
 		
 		while(this.running) {
 			Message message = null;
@@ -58,6 +81,9 @@ public abstract class AbstractSession extends Thread {
 				catch(InvalidMessageException e) {}
 			}
 		}
+		
+		for(OnSessionStatusListener l : this.on_session_status_listeners)
+			l.onSessionStopped(this);
 	}
 	
 	public abstract void send(Message message);
--- src/com/mwr/cinnibar/connection/AbstractSessionCollection.java
+++ src/com/mwr/cinnibar/connection/AbstractSessionCollection.java
@@ -3,7 +3,9 @@ package com.mwr.cinnibar.connection;
 import java.util.Collection;
 import java.util.HashMap;
 
-public abstract class AbstractSessionCollection {
+import com.mwr.cinnibar.connection.AbstractSession.OnSessionStatusListener;
+
+public abstract class AbstractSessionCollection implements OnSessionStatusListener {
 	
 	private HashMap<String,AbstractSession> sessions = new HashMap<String,AbstractSession>();
 	
@@ -19,6 +21,8 @@ public abstract class AbstractSessionCollection {
 	
 	protected AbstractSession storeSession(AbstractSession session) {
 		this.sessions.put(session.getSessionId(), session);
+		
+		session.addOnSessionStatusListener(this);
 		session.start();
 			
 		return session;
@@ -27,15 +31,13 @@ public abstract class AbstractSessionCollection {
 	public AbstractSession get(String session_id) {
 		return this.sessions.get(session_id);
 	}
-	
-	protected abstract void onSessionStarted(AbstractSession session);
-	
-	protected abstract void onSessionStopped(AbstractSession session);
 
 	public AbstractSession stop(String session_id) {
 		AbstractSession session = this.sessions.get(session_id);
 
 		if(session != null) {
+			this.sessions.remove(session_id);
+			
 			try {
 				session.stopSession();
 				session.join();
@@ -43,12 +45,8 @@ public abstract class AbstractSessionCollection {
 			catch(InterruptedException e) {
 				e.printStackTrace();
 			}
-
-			this.sessions.remove(session_id);
-				
-			this.onSessionStopped(session);
 		}
-			
+		
 		return session;
 	}
 
